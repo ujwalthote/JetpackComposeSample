c7d5f304b697f33cea7b7f5de2739f8bc4ec79d2
Ujwal Thote <thoteujwal97@gmail.com>
2022-05-29 13:31:06 +0530

    Add splash screen and initial setup

--- app/src/main/java/com/ujwal/grocerycomposesample/MainActivity.kt
+++ app/src/main/java/com/ujwal/grocerycomposesample/MainActivity.kt
@@ -3,13 +3,17 @@ package com.ujwal.grocerycomposesample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ujwal.grocerycomposesample.ui.theme.LightGreen
 import com.ujwal.grocerycomposesample.ui.theme.GroceryComposeSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +21,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GroceryComposeSampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                SplashScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SplashScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = LightGreen
+    ) {
+        Image(
+            modifier = Modifier
+                .wrapContentHeight()
+                .padding(horizontal = 80.dp),
+            painter = painterResource(id = R.drawable.ic_splash_logo),
+            contentDescription = "Logo"
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     GroceryComposeSampleTheme {
-        Greeting("Android")
+        SplashScreen()
     }
 }
--- app/src/main/java/com/ujwal/grocerycomposesample/ui/theme/Theme.kt
+++ app/src/main/java/com/ujwal/grocerycomposesample/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -17,14 +18,12 @@ private val LightColorPalette = lightColors(
     primaryVariant = Purple700,
     secondary = Teal200
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+//    background = Color.White,
+//    surface = Color.White,
+//    onPrimary = Color.White,
+//    onSecondary = Color.Black,
+//    onBackground = Color.Black,
+//    onSurface = Color.Black,
 )
 
 @Composable
