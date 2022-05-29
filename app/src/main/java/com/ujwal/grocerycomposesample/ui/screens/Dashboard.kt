f27ca554f047284ea3b871fe6cadb1cbeabf9aac
Ujwal Thote <thoteujwal97@gmail.com>
2022-05-29 21:44:07 +0530

    Optimise imports

--- app/src/main/java/com/ujwal/grocerycomposesample/ui/screens/Dashboard.kt
+++ app/src/main/java/com/ujwal/grocerycomposesample/ui/screens/Dashboard.kt
@@ -1,18 +1,9 @@
 package com.ujwal.grocerycomposesample.ui.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.BottomAppBar
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.ujwal.grocerycomposesample.ui.InitNavigation
