25d4979f9a707a7415b1abb650991ef0818bb907
Ujwal Thote <thoteujwal97@gmail.com>
2022-06-04 11:51:54 +0530

    Revert

--- app/src/main/java/com/ujwal/grocerycomposesample/ui/components/BottomNavigation.kt
+++ app/src/main/java/com/ujwal/grocerycomposesample/ui/components/BottomNavigation.kt
@@ -99,39 +99,3 @@ fun GroceryBottomNavigation(navController: NavController) {
 fun PreviewBottomNav() {
     GroceryBottomNavigation(navController = rememberNavController())
 }
-
-sealed class BottomNavItem(
-    @StringRes var title: Int,
-    @DrawableRes var icon: Int,
-    var screen_route: String
-) {
-    object Shop : BottomNavItem(
-        title = R.string.tab_title_shop,
-        icon = R.drawable.ic_shop,
-        screen_route = RouteHelper.Shop.route
-    )
-
-    object Explore : BottomNavItem(
-        title = R.string.tab_title_explore,
-        icon = R.drawable.ic_explore,
-        screen_route = RouteHelper.Explore.route
-    )
-
-    object Cart : BottomNavItem(
-        title = R.string.tab_title_cart,
-        icon = R.drawable.ic_cart,
-        screen_route = RouteHelper.Cart.route
-    )
-
-    object Favorite : BottomNavItem(
-        title = R.string.tab_title_favorite,
-        icon = R.drawable.ic_fav,
-        screen_route = RouteHelper.Favorite.route
-    )
-
-    object Account : BottomNavItem(
-        title = R.string.tab_title_account,
-        icon = R.drawable.ic_account,
-        screen_route = RouteHelper.Account.route
-    )
-}
