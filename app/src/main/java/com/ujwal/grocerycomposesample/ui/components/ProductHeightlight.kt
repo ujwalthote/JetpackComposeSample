4597aa340e83bff521daf93c88622efa3064b857
Ujwal Thote <thoteujwal97@gmail.com>
2022-07-12 00:29:42 +0530

    add padding

--- app/src/main/java/com/ujwal/grocerycomposesample/ui/components/ProductHeightlight.kt
+++ app/src/main/java/com/ujwal/grocerycomposesample/ui/components/ProductHeightlight.kt
@@ -1,31 +1,33 @@
 package com.ujwal.grocerycomposesample.ui.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.runtime.Composable
 import com.ujwal.grocerycomposesample.model.CategoryProducts
 import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ujwal.grocerycomposesample.R
 import com.ujwal.grocerycomposesample.model.Product
 
 @Composable
 fun ProductHeightlight(productCategory: CategoryProducts) {
-    Column {
+    Column(modifier = Modifier.padding(20.dp)) {
         Text(text = productCategory.title)
         productCategory.products?.let {
-            LazyRow{
-                items(it.size){index ->
+            LazyRow {
+                items(it.size) { index ->
                     ProductItem(product = it[index])
                 }
             }
-        }?: kotlin.run {
+        } ?: kotlin.run {
             Text(text = stringResource(R.string.content_not_found))
         }
     }
 }
-
 
 
 @Preview
