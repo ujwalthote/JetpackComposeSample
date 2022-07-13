d77e353943238cb4c47310730584a5b33cb81b0c
Ujwal Thote <thoteujwal97@gmail.com>
2022-07-14 01:42:09 +0530

    Adjust arrangement

--- app/src/main/java/com/ujwal/grocerycomposesample/ui/components/ProductItem.kt
+++ app/src/main/java/com/ujwal/grocerycomposesample/ui/components/ProductItem.kt
@@ -1,11 +1,14 @@
 package com.ujwal.grocerycomposesample.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,12 +25,14 @@ fun ProductItem(product: Product) {
             .height(248.dp)
             .padding(end = 15.dp)
             .border(1.dp, GrayBorder, RoundedCornerShape(20.dp))
-            .padding(15.dp)
+            .padding(15.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         AsyncImage(
             model = product.image,
             contentDescription = stringResource(R.string.content_desc_product_image),
-            modifier = Modifier
+            modifier = Modifier.height(124.dp),
+            alignment = Alignment.Center,
         )
         Text(
             text = product.title ?: "",
